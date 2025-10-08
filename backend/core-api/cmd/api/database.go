b0efe0f75fd3ad9415326579b5e57f4892e3a3e8
Poted <o.jpkmc@gmail.com>
2025-10-09 00:04:04 +0200

    refactor: contenerize API

--- backend/core-api/cmd/api/database.go
+++ backend/core-api/cmd/api/database.go
@@ -1,26 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/jmoiron/sqlx"
 )
 
-func connectToDB() *sqlx.DB {
-	dsn := "host=localhost port=5432 user=raitometer_user password=raitometer_password dbname=raitometer_db sslmode=disable"
-
+func connectToDB(dsn string) (*sqlx.DB, error) {
 	db, err := sqlx.Connect("pgx", dsn)
 	if err != nil {
-		log.Fatalf("could not connect to database: %v", err)
+		return nil, fmt.Errorf("could not connect to database: %v", err)
 	}
 
 	log.Println("database connection established successfully")
 
 	err = db.Ping()
 	if err != nil {
-		log.Fatalf("error pinging database: %v", err)
+		return nil, fmt.Errorf("error pinging database: %v", err)
 	}
 
-	return db
+	return db, nil
 }
--- backend/core-api/cmd/api/main.go
+++ backend/core-api/cmd/api/main.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/Poted/raitometer/backend/core-api/internal/server"
 )
 
 func main() {
-	dbConnection := connectToDB()
-	defer dbConnection.Close()
+	connStr := os.Getenv("DATABASE_URL")
+	if connStr == "" {
+		log.Fatal("DATABASE_URL environment variable is not set")
+	}
 
-	srv := server.New(dbConnection)
-
-	port := ":8080"
-	err := srv.Start(port)
+	db, err := connectToDB(connStr)
 	if err != nil {
-		log.Fatalf("fatal server error: %v", err)
+		log.Fatalf("could not connect to database: %v", err)
+	}
+	defer db.Close()
+	log.Println("database connection established successfully")
+
+	s := server.New(db)
+	err = s.Start(":8080")
+	if err != nil {
+		log.Fatalf("could not start server: %v", err)
 	}
 }
